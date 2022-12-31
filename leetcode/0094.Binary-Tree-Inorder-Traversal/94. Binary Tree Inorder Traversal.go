90893e481080072c727d7d31dea193c6ecbf57ef
ru7dy <rudymelody@gmail.com>
2022-12-31 01:00:18 -0800

    add iterative answer for 0094

--- leetcode/0094.Binary-Tree-Inorder-Traversal/94. Binary Tree Inorder Traversal.go
+++ leetcode/0094.Binary-Tree-Inorder-Traversal/94. Binary Tree Inorder Traversal.go
@@ -27,3 +27,30 @@ func inorder(root *TreeNode, output *[]int) {
         inorder(root.Right, output)
     }
 }
+
+func inorderTraversalInterative(root *TreeNode) []int {
+	var result []int
+	if root == nil {
+		return result
+	}
+
+	current := root 
+    
+	// stack := []*TreeNode{} 
+    stack := make([]*TreeNode, 0)    
+	
+    for len(stack) > 0 || current != nil {
+		if current != nil {
+			stack = append(stack, current)
+			current = current.Left
+		} else {
+			last_idx := len(stack) - 1             
+			current = stack[last_idx]
+			stack = stack[:last_idx]            
+			result = append(result, current.Val)
+			current = current.Right
+		}
+	}
+
+	return result
+}
